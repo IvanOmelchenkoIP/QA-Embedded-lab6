b195d37a07f2f8e595bb2cd82f34b94683c8fbf8
IvanOmelchenkoIP <io25112002@gmail.com>
2023-12-09 20:22:50 +0200

    Implement client function
    Implement client function to get stdout from iperf3

--- src/script.py
+++ src/script.py
@@ -2,9 +2,18 @@ import subprocess
 
 server_ip = "192.168.56.1"
 
+
 def client(server_ip):
-    p = subprocess.Popen(["iperf3", "-c", server_ip, "-i", "1"])
-    return 
+    p = subprocess.Popen(
+        ["iperf3", "-c", server_ip, "-i", "1"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    return p.communicate()
+
 
 def parser():
     return
+
+
+result, error = client(server_ip)
